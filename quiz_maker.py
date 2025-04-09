f71b91f2b3bdb79bcc2ee2eb68650159bf47381f
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-04-09 20:35:37 +0800

    adding a feature that allows user to edit content in a specificline. used for loop to repeat the process, and then enumerate to add 1 per line in the file. i specifically added 1 besides the lines variable so that the line number will start at 1.

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,22 +1,39 @@
 import os
+import fileinput
 
-# Append the user's input to a txt file
-def question_comp():
-    while True:
-        subjects = input('What subject? (math, science, english, history) or enter "quit": ').lower()
-        if subjects == "quit":
-            break
+class QuizMaker:
 
-        # Open file and append input to the text file
-        questionares = open(f"{subjects}.txt", 'a')
-        questionares.write(input('Enter a question: ') + '\n')
-        questionares.write('a) ' + input('Enter option a: ') + '\n')
-        questionares.write('b) ' + input('Enter option b: ') + '\n')
-        questionares.write('c) ' + input('Enter option c: ') + '\n')
-        questionares.write('d) ' + input('Enter option d: ') + '\n\n')
-        questionares.flush()
-        questionares.close() 
-        print('Questions are saved!')
-        print(f"Saved to: {os.path.abspath(subjects + '.txt')}")  # Show full file path
+    # Append the user's input to a txt file
+    def question_comp(self):
+        while True:
 
-question_comp()
+            # Asks the user to choose a subject
+            subjects = input('What subject? (math, science, english, history) or enter "quit": ').lower()
+            if subjects == "quit":  # Quit option 
+                break
+
+            # Open file and append input to the text file
+            questionares = open(f"{subjects}.txt", 'a')
+            questionares.write(input(f'Enter a question: ') + '\n')
+            questionares.write(f'a) {input('Enter option a: ')}\n')
+            questionares.write(f'b) {input('Enter option b: ')}\n')
+            questionares.write(f'c) {input('Enter option c: ')}\n')
+            questionares.write(f'd) {input('Enter option d: ')}\n\n')
+            questionares.write(f'What is the correct answer? {input(' a, b, c, or d?')} ')
+            questionares.flush()  # Forces pthon to write in the text file
+            questionares.close()  # Closes file
+            print('Questions are saved!')
+
+    def file_edit(self):
+        while True:
+            subjects = input('What subject? (math, science, english, history) or enter "quit": ').lower()
+            if subjects == "quit":
+                break
+            
+            # Opens file and then prints it for the user to see contents
+            questionares = open(f"{subjects}.txt, 'r'")
+            lines = questionares.readlines()
+            for i, line in enumerate(lines, 1):
+                print(f"{i}) {line.strip()}")
+            
+            
