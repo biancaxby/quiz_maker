c6bdf0bcef2dbfa123df657b40c2dc899c955e79
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-04-11 00:41:41 +0800

    switched continue to break, added missing parenthesis for when calling a function, also removed file input module cause i didnt really use it and removed the flush since the close function is enough. i thought at first that it was neccessary for my program as it was not working and was not saving the question to the txt file. also corrected the incorrect logic and variables.

--- quiz_maker.py
+++ quiz_maker.py
@@ -1,5 +1,4 @@
 import os
-import fileinput
 
 class QuizMaker:
 
@@ -11,16 +10,14 @@ class QuizMaker:
             subjects = input('What subject? (math, science, english, history) or enter "quit": ').lower()
             if subjects == "quit":  # Quit option 
                 break
-
             # Open file and append input to the text file
             questionares = open(f"{subjects}.txt", 'a')
-            questionares.write(input(f'Enter a question: ') + '\n')
-            questionares.write(f'a) {input('Enter option a: ')}\n')
-            questionares.write(f'b) {input('Enter option b: ')}\n')
-            questionares.write(f'c) {input('Enter option c: ')}\n')
-            questionares.write(f'd) {input('Enter option d: ')}\n\n')
-            questionares.write(f'What is the correct answer? {input(' a, b, c, or d?')} ')
-            questionares.flush()  # Forces pthon to write in the text file
+            questionares.write(input(f"Enter a question: \n"))
+            questionares.write(f"a) {input('Enter option a: ')}\n")
+            questionares.write(f"b) {input('Enter option b: ')}\n")
+            questionares.write(f"c) {input('Enter option c: ')}\n")
+            questionares.write(f"d) {input('Enter option d: ')}\n")
+            questionares.write(f'The correct answer is: {input('what is the right answer? a, b, c, or d? ')}\n\n ')
             questionares.close()  # Closes file
             print('Questions are saved!')
 
@@ -28,10 +25,10 @@ class QuizMaker:
         while True:
             subjects = input('What subject? (math, science, english, history) or enter "quit": ').lower()
             if subjects == "quit":
-                break
+                continue
             
             # Opens file and then prints it for the user to see contents
-            questionares = open(f"{subjects}.txt, 'r'")
+            questionares = open(f"{subjects}.txt", 'r')
             lines = questionares.readlines()
 
             # Adds numbering per line
@@ -43,7 +40,7 @@ class QuizMaker:
             print(questionares.read)
 
             # User will choose what line to edit
-            edit_line = input('What line would you like to change?(Enter 0 to cancel): ')
+            edit_line = int(input('What line would you like to change?(Enter 0 to cancel): '))
             if edit_line == 0:
                 break   
 
@@ -51,29 +48,27 @@ class QuizMaker:
             edit_statement = input(f"Enter new content on line {edit_line}: \n")
             
              # Replaces the line with new content
-            new_line = f"{edit_statement}\n"
+            lines[edit_line - 1] = f"{edit_statement}\n"
             
             # Writes on the file again
-            questionares = open(f"{subjects}.txt, 'w'")
-            questionares.writelines(new_line)
+            questionares = open(f"{subjects}.txt", 'w')
+            questionares.writelines(lines)
             questionares.close()
 
-    def view_questions():
+    def view_questions(self):
         while True:
             subjects = input('What subject? (math, science, english, history) or enter "quit": ').lower()
             if subjects == "quit":
-                break
-            questionares = open(f"{subjects}.txt, 'r'")
-            print(questionares.read)  # Prints all the questions stored on the text file
+                main_menu
+            questionares = open(f"{subjects}.txt", 'r')
+            print(questionares.read())  # Prints all the questions stored on the text file
 
-quiz = QuizMaker
+main_menu = QuizMaker()
 print('1. Create questions\n2. Edit questions\n3. See questions')
 user_input = input('What would you like to do?: ')
-if user_input == 1:
-    quiz.question_comp
-elif user_input == 2:
-    quiz.file_edit
-elif user_input == 3:
-    quiz.view_questions
-else:
-    print('Invalid input')
+if user_input == '1':
+    main_menu.question_comp()
+elif user_input == '2':
+    main_menu.file_edit()
+elif user_input == '3':
+    main_menu.view_questions()
