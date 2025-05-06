19a2ae072197dbacd016a1ebf8d4f849be4a12d7
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-06 19:41:57 +0800

    added a menu and used the next function to print the next line of the file. i also removed the list because i think its unneccessary

--- file_reader_for_quiz_maker.py
+++ file_reader_for_quiz_maker.py
@@ -4,21 +4,30 @@
 # Use startswith
 # Make sure only 4 of the lines are printed each time
 
-
-
 def file_reader():
     while True:
         subject = (input('What subject would you like?(math, english, science, history) ')).lower()
         quiz = open(f"{subject}.txt", 'r')
-        count_line = 0
+        count_line = 0   # Counts how many lines 
+
+        # Separates each line
         for line in quiz:
             print(line.strip())
-            count_line +=1
-            if count_line == 5:
-                input("do you want to continue? yes or no?")
-                if input == "yes":
-                    continue
-                if input == "no":
-                    break
+            count_line +=1      # Counts the line
 
+            # Checks if the lines have reached the max quantity
+            if count_line % 5 == 0:
+                input('What do you think is the answer? ')
+                next_line = next(quiz, None)
+                print(next_line)
+
+def menu():
+    while True:
+        user_input = input('Would you like to start now? (y/n) ').lower()
+        if user_input == "y":
+            file_reader()
+        else:
+            break
+ 
+menu()
 file_reader()
