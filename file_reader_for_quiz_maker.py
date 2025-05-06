9b04e2b02435bcb4e02b8097c92ece472c8a6f94
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-06 23:55:26 +0800

    i tried adding color to text when the answer is correct and used a different condition to identify if a line has the answers. also tried editing the for loop logic, only for me to change it back somehow.

--- file_reader_for_quiz_maker.py
+++ file_reader_for_quiz_maker.py
@@ -2,30 +2,43 @@
 # Pseudocode
 # Try to use the following: readlines, readfiles 
 # Use startswith
-# Make sure only 4 of the lines are printed each time
+# Make sure only 5 of the lines are printed each time
 
 def file_reader():
     while True:
+        
         subject = (input('What subject would you like?(math, english, science, history) ')).lower()
         quiz = open(f"{subject}.txt", 'r')
-        count_line = 0   # Counts how many lines 
+        lines = quiz.readlines()
+        count_line = 0         # Counts how many lines 
+        correct_answers = 0    # Counts how many correct answers 
+        answer_key = []        # Contains the correct answers
 
         # Separates each line
-        for line in quiz:
+        for line in lines:
             print(line.strip())
             count_line +=1      # Counts the line
-
-            # Checks if the lines have reached the max quantity
-            if count_line % 5 == 0:
-                input('What do you think is the answer? ')
-                next_line = next(quiz, None)
-                print(next_line)
-
+            if count_line % 5 == 0:    # Checks if the lines have reached the max quantity
+                user_answer = input('What do you think is the answer? ').strip
+            
+            # Checks if the line is the answer key
+            if count_line < len(lines) and lines[count_line].startswith("The correct answer is:"):
+                answer = lines[count_line].split(":")[1].strip()   # Separates the answer
+                count_line += 1
+                
+            if  user_answer == answer:
+                print("\033[32mCorrect!!\033[0m")
+                correct_answers += 1
+            
 def menu():
     while True:
-        user_input = input('Would you like to start now? (y/n) ').lower()
-        if user_input == "y":
+        print('1. Start quiz')
+        print('2. View scores')
+        user_input = input("What would you like to do? ")
+        if user_input == "1":
             file_reader()
+        if user_input == "2":
+            continue
         else:
             break
  
