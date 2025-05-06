3ea7ce4d93dba867807074d5b831687832979752
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-07 01:49:15 +0800

    fixing the logic of the code since i want to only display only 5 lines of the file and i want to excllude the answer key and then skip over the blank lines. i also forgot to mention in my previous commit that i made changes to the menu function. i added the start quiz option where you will start the quiz and then view your score option.

--- file_reader_for_quiz_maker.py
+++ file_reader_for_quiz_maker.py
@@ -4,32 +4,44 @@
 # Use startswith
 # Make sure only 5 of the lines are printed each time
 
+
 def file_reader():
     while True:
         
         subject = (input('What subject would you like?(math, english, science, history) ')).lower()
-        quiz = open(f"{subject}.txt", 'r')
-        lines = quiz.readlines()
-        count_line = 0         # Counts how many lines 
-        correct_answers = 0    # Counts how many correct answers 
-        answer_key = []        # Contains the correct answers
+        try:
+            quiz = open(f"{subject}.txt", 'r')
+            lines = quiz.readlines()
+            count_line = 0         # Counts how many lines 
+            correct_answers = 0    # Counts how many correct answers 
+            question_count = 0
 
-        # Separates each line
-        for line in lines:
-            print(line.strip())
-            count_line +=1      # Counts the line
-            if count_line % 5 == 0:    # Checks if the lines have reached the max quantity
-                user_answer = input('What do you think is the answer? ').strip
-            
-            # Checks if the line is the answer key
-            if count_line < len(lines) and lines[count_line].startswith("The correct answer is:"):
-                answer = lines[count_line].split(":")[1].strip()   # Separates the answer
-                count_line += 1
+            while count_line < len(lines):
+                question_storage = []         # Stores the qestions here so that it is easier to deal with
+                if lines.isspace:      # Checks if the line is empty or only has space
+                    count_line += 1   
+                for i in range(5):
+                    if count_line + i < len(lines):
+                            question_count.append(lines[question_count + i].strip())
+
+            # Separates each line
+            for line in lines:
+                print(line.strip())
+                count_line +=1      # Counts the line
                 
-            if  user_answer == answer:
-                print("\033[32mCorrect!!\033[0m")
-                correct_answers += 1
-            
+                user_answer = input('What do you think is the answer? ').strip
+                
+                # Checks if the line is the answer key
+                if count_line < len(lines) and lines[count_line].startswith("The correct answer is:"):
+                    answer = lines[count_line].split(":")[1].strip()   # Separates the answer
+                    count_line += 1
+                    
+                if  user_answer == answer:
+                    print("\033[32mCorrect!!\033[0m")
+                    correct_answers += 1
+        except ValueError:
+            print('Invalid input')
+                
 def menu():
     while True:
         print('1. Start quiz')
@@ -42,5 +54,6 @@ def menu():
         else:
             break
  
-menu()
+
 file_reader()
+menu()
