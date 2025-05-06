7c36fd8a4874399f8b25571da96c64ee68199a40
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-06 17:29:30 +0800

    added the open file command to open the file which the user wants to to use. also added to line stip to separate each liens and added a counter to count how many lines have been displayed. i wanted to make sure that only 5 lines of the file is displayed so that the user does not see the answer and the other questions.

--- file_reader_for_quiz_maker.py
+++ file_reader_for_quiz_maker.py
@@ -1,1 +1,24 @@
 # Quiz file reader 
+# Pseudocode
+# Try to use the following: readlines, readfiles 
+# Use startswith
+# Make sure only 4 of the lines are printed each time
+
+
+
+def file_reader():
+    while True:
+        subject = (input('What subject would you like?(math, english, science, history) ')).lower()
+        quiz = open(f"{subject}.txt", 'r')
+        count_line = 0
+        for line in quiz:
+            print(line.strip())
+            count_line +=1
+            if count_line == 5:
+                input("do you want to continue? yes or no?")
+                if input == "yes":
+                    continue
+                if input == "no":
+                    break
+
+file_reader()
