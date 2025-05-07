c3c6c75a4c574f352b1bc316005db83d9731d83a
Paula Bianca Piquero <paulabiancapiquero@gmail.com>
2025-05-08 01:29:46 +0800

    the code does work on the first five lines. also i wanted the program to skip over the lines with blank spaces. it did work on the first five lines but when i proceedded with the next qeustions it didnt skip over the blank lines which made the code display the unwated content. also, in the next set of questions it printed out both the blank line and the line with the answer which is not indended.

--- file_reader_for_quiz_maker.py
+++ file_reader_for_quiz_maker.py
@@ -14,29 +14,30 @@ def file_reader():
             lines = quiz.readlines()
             count_line = 0         # Counts how many lines 
             correct_answers = 0    # Counts how many correct answers 
-
-            while count_line < len(lines):
-                for i in range(5):
-                    if count_line < len(lines):
-                            striped_lines = lines[count_line].strip()
+            answer_key = []
+          
+            for _ in range(5):
+                if count_line < len(lines):
+                        striped_lines = lines[count_line].strip()
+                        if striped_lines == "":
                             count_line += 1
-                
-                user_answer = input('What do you think is the answer? ').strip
-                if  user_answer == answer:
-                    print("\033[32mCorrect!!\033[0m")
-                    correct_answers += 1
+                            continue
+                        print(striped_lines)
+                        count_line += 1
 
-                # Checks if the line is the answer key
+             # Checks if the line is the answer key
                 if count_line < len(lines) and lines[count_line].startswith("The correct answer is:"):
-                    answer = lines[count_line].replace("The correct answer is: ", "").strip   # Separates the answer
+                    answer = lines[count_line].replace("The correct answer is: ", "").strip()   # Separates the answer
+                    answer_key.append(answer)
                     count_line += 1
 
-                if lines == "":
-                    continue_msg = input('want to continue? (y/n) ')
-                    if continue_msg == "y":
-                        return 
-                    if continue_msg == "n":
-                        menu()
+            user_answer = input('What do you think is the answer? ').lower().strip()
+            if  user_answer == answer_key:
+                    print("correct!")
+                    correct_answers += 1
+
+            if user_answer == "quit":
+                    menu()
 
         except ValueError:
             print('Invalid input')
